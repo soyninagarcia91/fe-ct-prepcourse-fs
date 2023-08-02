68be10ba2794ee4c678fe19d6cc2ccaf3195531d
nina <soyninagarcia91@gmail.com>
2023-08-01 23:48:51 -0300

    curso agosto

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = 'hola';
 
 // Crea una variable de tipo number.
 const nuevoNumero = null;
